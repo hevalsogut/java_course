f96b4f0f2451d45f2e4f5bac91bac4b71c52b92d
Heval Söğüt <hevalsogut@gmail.com>
2023-10-20 16:28:37 +0300

    Homework 1

---
+++ ITPLab1.java
@@ -0,0 +1,32 @@
+package java_course;
+
+public class ITPLab1 {
+  public static void main(String[] args) {
+    //Q1
+    System.out.print("it's my first project");
+    System.out.println("hello world");
+    //hello world is a string because of that it should be writen between two quotation marks
+    System.out.println("I love Java");
+    //There was a syntax error at the end of line
+
+    //Q2
+    int i = 0;
+    while (i < 5) {
+        System.out.println("I love Java");
+        i = i + 1;
+    }
+
+    //Q3
+    int x = 16;
+    System.out.println(x * 5);
+    System.out.println(x / 2);
+    System.out.println(Math.pow(x, 2));
+    System.out.println(Math.pow(x, 3));
+
+    //Q4
+    int r = 5;
+    System.out.println("Area = " + (Math.PI * Math.pow(r, 2)));
+
+  }
+}
+

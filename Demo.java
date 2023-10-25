3076eb723ae9b5425e8b396459d658d4904ba00a
Heval Söğüt <hevalsogut@gmail.com>
2023-10-25 15:19:43 +0300

    Lab Demo Interestrate

--- Demo.java
+++ Demo.java
@@ -2,36 +2,55 @@ import java.util.Scanner;
 
 public class Demo {
   public static void main(String[] args) {
-    char grade = 'f';
-    switch (grade) {
-      case 'A':
-        System.out.println("Mükemmel Geçtiniz");
-        break;
-
-      case 'B':
-        System.out.println("Çok güzel Geçtiniz");
-        break;
-
-      case 'C':
-        System.out.println("İyi Geçtiniz");
-        break;
-
-      case 'D':
-        System.out.println("Fena Değil Geçtiniz");
-        break;
-
-      case 'F':
-        System.out.println("Failed");
-        break;
-
-      default:
-        System.out.println("Geçersiz Not girdiniz");
-
-        break;
-
+    // For
+    for (int i = 1; i < 10; i += 2) {
+      System.out.println(i);
     }
+
+    System.out.println("Döngü Bitti.");
+
+    // While
+    int i = 1;
+    while (i < 10) {
+      System.out.println("i sayısı ondan küçüktür");
+      break;
+    }
+
   }
 }
+
+/*
+ * 
+ * char grade = 'f';
+ * switch (grade) {
+ * case 'A':
+ * System.out.println("Mükemmel Geçtiniz");
+ * break;
+ * 
+ * case 'B':
+ * System.out.println("Çok güzel Geçtiniz");
+ * break;
+ * 
+ * case 'C':
+ * System.out.println("İyi Geçtiniz");
+ * break;
+ * 
+ * case 'D':
+ * System.out.println("Fena Değil Geçtiniz");
+ * break;
+ * 
+ * case 'F':
+ * System.out.println("Failed");
+ * break;
+ * 
+ * default:
+ * System.out.println("Geçersiz Not girdiniz");
+ * 
+ * break;
+ * 
+ * }
+ */
+
 // public static void main(String[] args) {
 // int sayi1 = 20;
 // int sayi2 = 25;
---
+++ ITPLab3.java
@@ -0,0 +1,44 @@
+
+import java.util.Scanner;
+
+public class ITPLab3 {
+  public static void main(String[] args) {
+    double total = 0;
+    Scanner amountIncome = new Scanner(System.in);
+    System.out.print("Enter your income: ");
+    double income = amountIncome.nextDouble();
+
+    for (int i = 0; i < 6; i++) {
+      total += income;
+      total *= 1.1;
+      System.out.println("Account balance :" + total);
+    }
+  }
+}
+
+/*
+ * total = (income + total) * 1.1;
+ * System.out.println(total);
+ * total = (income + total) * 1.1;
+ * System.out.println(total);
+ * total = (income + total) * 1.1;
+ * System.out.println(total);
+ * total = (income + total) * 1.1;
+ * System.out.println(total);
+ * total = (income + total) * 1.1;
+ * System.out.println(total);
+ * total = (income + total) * 1.1;
+ * System.out.println(total);
+ */
+
+/*
+ * double total = 0;
+ * Scanner amountIncome = new Scanner(System.in);
+ * System.out.print("Enter your income: ");
+ * double income = amountIncome.nextDouble();
+ * 
+ * for (int i = 0; i < 6; i++) {
+ * total += income;
+ * total *= 1.1;
+ * System.out.println("Account balance :" + total);
+ */

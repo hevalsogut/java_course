f532b7eece942e14016f1baafc098c1a341bce17
Heval Söğüt <hevalsogut@gmail.com>
2023-12-27 14:32:01 +0300

    Random Array

---
+++ itp_lab/itp_lab.java
@@ -0,0 +1,35 @@
+package itp_lab;
+
+import java.util.Scanner;
+
+public class itp_lab {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the row value :");
+    int row = sc.nextInt();
+    System.out.print("Enter the column value :");
+    int col = sc.nextInt();
+    printArray(createArray(row, col));
+
+  }
+
+  public static int[][] createArray(int row, int col) {
+    int[][] myArray = new int[row][col];
+    for (int i = 0; i < row; i++) {
+      for (int j = 0; j < col; j++) {
+        myArray[i][j] = (int) (Math.random() * 11);
+      }
+    }
+    return myArray;
+  }
+
+  public static void printArray(int[][] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = 0; j < arr[i].length; j++) {
+        System.out.print(arr[i][j] + " ");
+      }
+      System.out.println(" ");
+    }
+  }
+
+}
